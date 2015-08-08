c87c0d233960f5943f8c7446682bacf1ed419d84
Saidou GUEYE <saidou.gueye@gmail.com>
2015-08-07 20:14:38 -0400

    Make the DuplicateHeadersException class store the duplicate headers and add an accessor for them

--- src/Exception/DuplicateHeadersException.php
+++ src/Exception/DuplicateHeadersException.php
@@ -7,11 +7,18 @@ namespace Ddeboer\DataImport\Exception;
  */
 class DuplicateHeadersException extends ReaderException
 {
+    private $duplicates;
     /**
      * @param array $duplicates
      */
     public function __construct(array $duplicates)
     {
         parent::__construct(sprintf('File contains duplicate headers: %s', implode($duplicates, ', ')));
+        $this->duplicates = $duplicates;
+    }
+    
+    public function getDuplicates()
+    {
+        return $this->duplicates;
     }
 }
